799ec68885b134e025545554e5fc611cd20103b2
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-12 07:59:04 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  i = 0
+  squared_array = []
+
+  while i < array.length
+    squared_array << array[i] ** 2
+    i += 1
+  end
+  squared_array
+
 end
